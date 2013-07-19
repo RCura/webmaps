b51e11fe5a251b329d83554e8639bcad990c8277
Robin Cura <robin.cura@gmail.com>
2013-07-19 16:47:46 +0200

    cosmethic

--- test/test.R
+++ test/test.R
@@ -7,14 +7,14 @@ file.remove(Sys.glob(paths="./test/index.html"))
 
 state <- data.frame(state.x77)
 state$Name <- rownames(state)
+state$color <- strtrim(rainbow(n=nrow(state)), 7)
 coordinates(state) <- cbind(state.center$x, state.center$y)
 
-osmMap ( layer ( state , "States" ,
-                 lstyle (pointRadius = '${Murder}',
-                         fillColor = "${Frost}" ,
-                         strokeColor = "black",
-                         fillOpacity = 0.4)
-                 ),
-title = "State Data" ,
-         outputDir = "./test/" )
 
+osmMap(layer(layerData = state ,name = "States",lstyle(
+        pointRadius = '${Murder}',
+        fillColor = "${color}" ,
+        strokeColor = "black",
+        fillOpacity = 0.4)),
+    title = "State Data" ,
+    outputDir = "./test/")
