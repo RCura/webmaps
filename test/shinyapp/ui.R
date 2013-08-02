eb064ff6c9dee426a030a44af0f3beb36d523511
RCura <robin.cura@gmail.com>
2013-08-02 14:32:51 +0200

    ibid

--- test/shinyapp/ui.R
+++ test/shinyapp/ui.R
@@ -18,7 +18,7 @@ shinyUI(pageWithSidebar(
     ),
     mainPanel(
             htmlOutput('webmap'),
-            div(id='map', style='width: 500px ;height: 400px'),
+            div(id='map', style='width: 100% ;height: 500px'),
             div(id='status')
     )
 ))
